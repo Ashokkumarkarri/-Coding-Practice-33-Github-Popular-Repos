68a1554c67381604ff302e604146fdfda9730ba0
Ashok kumar <ashok.ashok1999@gmail.com>
2025-07-13 22:04:46 +0000

    Initial commit

---
+++ src/components/GithubPopularRepos/index.js
@@ -0,0 +1,7 @@
+const languageFiltersData = [
+  {id: 'ALL', language: 'All'},
+  {id: 'JAVASCRIPT', language: 'Javascript'},
+  {id: 'RUBY', language: 'Ruby'},
+  {id: 'JAVA', language: 'Java'},
+  {id: 'CSS', language: 'CSS'},
+]
